f3d3410d6edb2493ac49750f9bb0757873e58e76
Radosław Przytuła <radoslaw.przytula@gmail.com>
2018-01-22 15:25:30 +0100

    rename scripts

--- binance-trading-bot/check_prices.py
+++ binance-trading-bot/check_prices.py
@@ -1,54 +1,26 @@
 #!/usr/bin/env python3
 
 import os
-import time
-import json
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_core.settings')
-
 import datetime
-from decimal import Decimal
 from django.conf import settings
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
-from binance.exceptions import BinanceAPIException
+
 from pymongo import MongoClient
-
-from _core.asgi import channel_layer
-
 
 client = MongoClient()
 db = client['binance']
 prices = db.prices
-watched = db.watched
-binance_client = Client(settings.BINANCE_API_KEY, settings.BINANCE_SECRET)
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_core.settings')
 
-while True:
-    payload = {}
-    for coin_watched in watched.find():
-        current_price = prices.find_one({"s": coin_watched['s']})['c']
-        buy_price = Decimal(coin_watched['buy'])
-        change = round((Decimal(current_price) - buy_price) / buy_price \
-                                                                * 100, 2)
-        if change >= int(coin_watched['sell']) or \
-           change <= -int(coin_watched['stop']):
+c = Client(settings.BINANCE_API_KEY, settings.BINANCE_SECRET)
 
-            watched.delete_one({"tc": coin_watched['tc']})
-            payload[int(coin_watched['tc'])] = ["SOLD", str(change)]
+def process_message(payload):
+	for coin in payload:
+		prices.find_one_and_update({"s": coin['s']}, {"$set": 
+										  {"c": str(coin['c'])}}, upsert=True)
 
-            try:
-                o_r = binance_client.create_order(symbol=coin_watched['s'],
-                                                  side='SELL',
-                                                  type='MARKET',
-                                                  quantity=coin_watched['q'])
-                print(o_r)
-
-            except BinanceAPIException as e:
-                print(binance_client.get_account())
-                print(e, coin_watched)
-        else:
-            payload[int(coin_watched['tc'])] = [current_price, str(change)]
-
-    channel_layer.send("prices", {"text": json.dumps(payload)})
-    time.sleep(0.5)
+bm = BinanceSocketManager(c)
+bm.start_ticker_socket(process_message)
+bm.start()
